50ce4a3414b8cafe2a5a8157093d842693c57716
Johnny Bowman <JohnnyBowman@gmail.com>
2013-03-28 20:27:20 -0400

    Can count how stops till Union Square and from Unions Square til endpoint for single line

--- NYCSubway.rb
+++ NYCSubway.rb
@@ -1,23 +1,58 @@
 
 nline = ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"]
+lline = ["8th", "6th","Union Square", "3rd", "1st"]
+sixline = ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
 
 puts "What stop are you getting on at?"
 gets_on = gets.strip.chomp
+puts "What line is that"
+on_line = gets.strip.chomp
 
+puts "What stop are you getting off at?"
+gets_off = gets.strip.chomp
+puts "What line is that"
+off_line = gets.strip.chomp
+
+#find position of starting point subwy stop in array
 i = 0
 while nline[i] != gets_on
 	i = i + 1
 end
-puts i
+starting_point = i
 
-puts "What stop are you getting off at?"
-gets_off = gets.strip.chomp
-
+#find position of getting off point subway stop in array
 n = 0
-while nline[n] != gets_off
+while nline[n] != gets_off && nline[n] != "Union Square"
 	n = n + 1
 end
-puts n
+ending_point = n
+
+#find difference in stops between starting and ending points
+leg1_number = (starting_point - ending_point).abs
+puts "Your subway stop is #{leg1_number} stops away"
+
+#find number of stops from Union Station to destination
+g = 0
+while nline[g] != gets_off
+	g = g + 1
+end
+leg2_number = (4 - g).abs 
+puts "Your subway stop is #{leg2_number} stops away"
+
+
+
+
+
+# while nline[starting_point] != nline[ending_point]
+# 	q = q
+
+
+
+# n = 0
+# while nline[n] != gets_off
+# 	n = n + 1
+# end
+# puts n
 
 
 
