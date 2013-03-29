1c8f9a4f813d51b3d9cfbb95d4d6fb325dad5500
Johnny Bowman <JohnnyBowman@gmail.com>
2013-03-29 00:07:36 -0400

    Now spits out total number of subways stops for all subway lines

--- NYCSubway.rb
+++ NYCSubway.rb
@@ -6,38 +6,73 @@ sixline = ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place
 puts "What stop are you getting on at?"
 gets_on = gets.strip.chomp
 puts "What line is that"
-on_line = gets.strip.chomp
+line_one = gets.strip.chomp
+
+case line_one
+	when "nline"
+		line_one = nline
+	when "lline"
+		line_one = lline
+	when "sixline"
+		line_one = sixline
+end
 
 puts "What stop are you getting off at?"
 gets_off = gets.strip.chomp
 puts "What line is that"
-off_line = gets.strip.chomp
+line_two = gets.strip.chomp
+
+case line_two
+	when "nline"
+		line_two = nline
+	when "lline"
+		line_two = lline
+	when "sixline"
+		line_two = sixline
+end
 
 #find position of starting point subwy stop in array
 i = 0
-while nline[i] != gets_on
+
+while line_one[i] != gets_on
 	i = i + 1
+
 end
 starting_point = i
+#puts i
 
 #find position of getting off point subway stop in array
 n = 0
-while nline[n] != gets_off && nline[n] != "Union Square"
+while (line_one[n] != gets_off) && (line_one[n] != "Union Square")
 	n = n + 1
 end
 ending_point = n
+#puts n
 
 #find difference in stops between starting and ending points
 leg1_number = (starting_point - ending_point).abs
-puts "Your subway stop is #{leg1_number} stops away"
+#puts "Your subway stop is #{leg1_number} stops away"
+
+#find starting point of Unions Square
+u = 0
+while line_two[u] != "Union Square"
+	u = u + 1
+end
+us_position = u
+#puts u
 
 #find number of stops from Union Station to destination
 g = 0
-while nline[g] != gets_off
+while line_two[g] != gets_off
 	g = g + 1
 end
-leg2_number = (4 - g).abs 
-puts "Your subway stop is #{leg2_number} stops away"
+#puts g
+leg2_number = (us_position - g).abs 
+#puts "Your subway stop is #{leg2_number} stops away"
+
+total_stops = leg1_number + leg2_number
+
+puts "#{gets_off} is #{total_stops} stops away"
 
 
 
