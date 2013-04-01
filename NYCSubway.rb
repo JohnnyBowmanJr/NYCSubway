22ee465c37583fce1968fd05abe8f234ec48ae42
Johnny Bowman <JohnnyBowman@gmail.com>
2013-03-31 20:10:39 -0400

    replaced loops with index Array methods

--- NYCSubway.rb
+++ NYCSubway.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 nline = ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"]
 lline = ["8th", "6th","Union Square", "3rd", "1st"]
@@ -33,14 +34,11 @@ case line_two
 end
 
 #find position of subway stop person gets on at in array
-i = 0
-while line_one[i] != gets_on
-	i = i + 1
-end
-starting_point = i
 
-#find position of subway stop person gets off at in array
-n = 0
+starting_point = line_one.index(gets_on)
+
+#find position of subway stop person gets off (for Union station or destination) in array
+n=0
 while (line_one[n] != gets_off) && (line_one[n] != "Union Square")
 	n = n + 1
 end
@@ -50,61 +48,19 @@ ending_point = n
 leg1_number = (starting_point - ending_point).abs
 
 #find starting point of Union Square
-u = 0
-while line_two[u] != "Union Square"
-	u = u + 1
-end
-us_position = u
+us_position = line_two.index("Union Square")
 
 #find number of stops from Union Station to destination
-g = 0
-while line_two[g] != gets_off
-	g = g + 1
-end
-
-#if person had to switch at Union Square, calculate the number of stops in leg 2. If person doesn't get off, 
-#don't calculate leg 2.
 if line_one[n] == "Union Square"
-	leg2_number = (us_position - g).abs
+	leg2_number = (us_position - line_two.index(gets_off)).abs
 else
 	leg2_number = 0
 end
 
+#if person had to switch at Union Square, calculate the number of stops in leg 2. If person doesn't get off, 
+#don't calculate leg 2.
+
+
 total_stops = leg1_number + leg2_number
 
 puts "#{gets_off} is #{total_stops} stops away"
-
-
-
-
-
-# while nline[starting_point] != nline[ending_point]
-# 	q = q
-
-
-
-# n = 0
-# while nline[n] != gets_off
-# 	n = n + 1
-# end
-# puts n
-
-
-
-
-# puts "What stop are you getting off at?"
-# get_off = gets.strip.chomp
-
-# while gets_on != gets_off
-#   NLine[i]
-#   i = i +1
-# end
-# puts "#{i} stops"
-
-# NLine.each do |stop|
-# 	i = 0
-# 	while stop != gets_off
-# 		i = i +1
-# 	end
-# 	puts "#{i} stops"
-# end
